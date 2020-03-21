fd67cbacccfb7712691d9911a63f0e6de0e9e311
Marek <kapsel521@gmail,com>
2020-03-21 14:37:11 +0100

    downloading notes from db

--- app/src/main/java/pl/mkonkel/wsb/firebasedb/MainActivity.kt
+++ app/src/main/java/pl/mkonkel/wsb/firebasedb/MainActivity.kt
@@ -15,14 +15,12 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val db = FirebaseDatabase.getInstance().reference
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        databadeListener()
-        button_add_note.setOnClickListener{
+        getSavedNotes()
+        databaseListener()
+        button_add_note.setOnClickListener {
             val title = note_title.text.toString()
             val body = note_body.text.toString()
             addNote(title, body)
@@ -34,56 +32,77 @@ class MainActivity : AppCompatActivity() {
             title = title,
             message = message
         )
-
         val uuid = UUID.randomUUID().toString()
-
         db.child(NOTE)
             .child(uuid)
             .setValue(note)
             .addOnSuccessListener {
-                Timber.i("succesfull note adding")
+                Timber.i("Successful note adding")
             }
             .addOnFailureListener {
-                Timber.e("Faliure during adding note")
+                Timber.e("Failure during adding Note")
             }
-
     }
 
-    private fun databadeListener(){
+    private fun databaseListener() {
         db.child(NOTE)
-            .addValueEventListener(
-                object : ValueEventListener{
-                    override fun onCancelled(p0: DatabaseError) {
-                        Timber.e("request was canceled")
-                    }
-
-                    override fun onDataChange(p0: DataSnapshot) {
-                    Timber.i( "something was changed")
-                    }
-
+            .addValueEventListener(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    Timber.e("Request was Canceled!")
                 }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    val notes = p0.children.mapNotNull {
+                        it.getValue(Note::class.java)
+                    }
+                    updateView(notes)
+                }
+            }
             )
     }
 
+    private fun getSavedNotes() {
+        db.child(NOTE)
+            .limitToFirst(100)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onCancelled(p0: DatabaseError) {
+                    Timber.e("Request was Canceled!")
+                }
 
-//    TODO: Add DatabaseListener
-//    Get child("childName") - first branch
-//    Get child("branchName") - get element in this branch
-//    addValueEventListener
-//
-//    onDataChane() will be invoked when any data will be modified in whole child branch.
-//    You can print changed data in the log using Timber.i(data)
+                override fun onDataChange(p0: DataSnapshot) {
+                    Timber.i("Something was Changed!")
+                    val notes = p0.children.mapNotNull {
+                        it.getValue(Note::class.java)
+                    }
+                    updateView(notes)
+                }
+            })
+    }
 
-//    TODO: Add Helper method for adding "news" to the DB.
-//    child() - go to notes branch
-//    child() - go to element in branch (ec. User One notes).
-//    child() - value in branch
-//    setValue(givenValue)
-//
-//    You can additional add onSuccess and onFailureListener
+    fun updateView(notes: List<Note>) {
+        val joinedNotes = notes.joinToString("\n\n")
+        notes_preview.text = ""
+        notes_preview.text = joinedNotes
+    }
 
-
-    companion object{
+    companion object {
         const val NOTE = "note"
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
